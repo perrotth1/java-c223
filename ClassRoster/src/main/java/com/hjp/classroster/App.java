670e7d4d7118456e64c13475e80764f6a644e7da
perrotth1 <perrotth1@hawkmail.newpaltz.edu>
2022-04-13 14:47:09 -0400

    converted to use spring

--- ClassRoster/src/main/java/com/hjp/classroster/App.java
+++ ClassRoster/src/main/java/com/hjp/classroster/App.java
@@ -4,18 +4,21 @@ import com.hjp.classroster.ui.*;
 import com.hjp.classroster.dao.*;
 import com.hjp.classroster.controller.*;
 import com.hjp.classroster.service.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 /**
  *
  * @author Henry
  */
 public class App {
     public static void main(String[] args){
-        UserIO io = new UserIOConsoleImpl();
-        ClassRosterView view = new ClassRosterView(io);
-        ClassRosterDao dao = new ClassRosterDaoFileImpl();
-        ClassRosterAuditDao audit = new ClassRosterAuditDaoFileImpl();
-        ClassRosterServiceLayer service = new ClassRosterServiceLayerImpl(dao, audit);
-        ClassRosterController controller = new ClassRosterController(view, service);
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+        
+        //instantiate object from bean. First param is id of bean to get, second is what type of 
+        //class this will be instantiating
+        ClassRosterController controller = ctx.getBean("controller", ClassRosterController.class);
         controller.run();
+        
+        
     }
 }
--- ClassRoster/src/test/java/com/hjp/classroster/service/ClassRosterServiceLayerImplTest.java
+++ ClassRoster/src/test/java/com/hjp/classroster/service/ClassRosterServiceLayerImplTest.java
@@ -7,12 +7,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  *
  * @author Henry
  */
 public class ClassRosterServiceLayerImplTest {
+    
+    ApplicationContext ctx = 
+        new ClassPathXmlApplicationContext("applicationContext.xml");
+    ClassRosterServiceLayer service = 
+        ctx.getBean("serviceLayer", ClassRosterServiceLayer.class);
     
     public ClassRosterServiceLayerImplTest() {
     }
